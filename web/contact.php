bd13c6fd796927208061898f8c1900cb82b07625
bishopblade <rj7300@gmail.com>
2013-03-25 19:01:03 -0700

    Initial commit

---
+++ web/contact.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Contact Us!</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body id="contact">
+		<div class="navbar">
+			<ul>
+				<li><div class="navbar2">Chaotic Ascension</div></li>
+				<div id="menu">
+					<li><a id="nav-home" href="index.php">Home</a></li>
+					<li><a id="nav-download" href="download.php">Download</a></li>
+					<li><a id="nav-newsblog" href="http://chaoticascension.tumblr.com">News/Blog</a></li>
+					<li><a id="nav-contact" href="contact.php">Contact</a></li>
+				</div>
+			</ul>
+		</div>
+	</body>
+</html>
---
+++ web/creators.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>The Creators of Chaotic Ascension</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="navbar"><div id="navbar2">CHAOTIC ASCENSION</div></div>
+		<div class="content">
+			<ul>
+				<li>Rowan-James Tran(BishopBlade)</li>
+				<li>Sasha(Sushieater888)</li>
+			</ul>
+		</div>
+	</body>
+</html>
